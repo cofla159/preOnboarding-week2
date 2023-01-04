ea26d0cfc2d1a7fca9b478a6a5904b3980e86a5d
cofla <cofla159@naver.com>
2023-01-05 00:44:21 +0900

    feat: 이슈 상태칸 추가

--- app/src/components/Board.js
+++ app/src/components/Board.js
@@ -14,6 +14,7 @@ function Board() {
         dueDate: e.target.dueDate.value,
         assignedTo: e.target.assignedTo.value,
         content: e.target.content.value,
+        state: e.target.state.value,
       })
     );
     setShowModal(false);
@@ -35,17 +36,12 @@ function Board() {
             <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
               <div className="relative w-auto my-6 mx-auto max-w-3xl">
                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                  <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                    <h3 className="text-3xl font-semibold">새 이슈</h3>
-                    <button
-                      className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                      onClick={() => setShowModal(false)}
-                    >
-                      <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                        x
-                      </span>
-                    </button>
-                  </div>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="text-black h-6 w-6 text-2xl block">x</span>
+                  </button>
                   <div className="relative p-6 flex-auto">
                     <form className="space-y-6" onSubmit={onSubmit}>
                       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -73,6 +69,24 @@ function Board() {
                           name="assignedTo"
                           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                         />
+                      </label>
+
+                      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        상태
+                        <select
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                          name="state"
+                        >
+                          <option className="hover:bg-yellow" value="todo">
+                            할 일
+                          </option>
+                          <option className="hover:bg-yellow" value="doing">
+                            진행 중
+                          </option>
+                          <option className="hover:bg-yellow" value="done">
+                            완료
+                          </option>
+                        </select>
                       </label>
 
                       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
